deb878a9e3cad515927b9eaee018d8b91345a5d8
Minminminwoo <alsdnsms07@naver.com>
2025-05-07 09:37:57 +0900

    refactor: 프론트 리다이렉트 페이지 경로application.yaml에서 관리하도록 수정

--- src/main/java/com/playus/userservice/domain/oauth/handler/CustomSuccessHandler.java
+++ src/main/java/com/playus/userservice/domain/oauth/handler/CustomSuccessHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -25,6 +26,9 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     private final JwtUtil jwtUtil;
     private final RedisTemplate<String, String> redisTemplate;
+
+    @Value("${app.frontend.redirect-uri}")
+    private String redirectUri;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
@@ -67,7 +71,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
             response.setHeader("Access-Control-Expose-Headers", "Authorization");
 
             // 로그인 후 프론트 페이지로 리다이렉트
-            getRedirectStrategy().sendRedirect(request, response, "http://localhost:3000/choice-team");
+            getRedirectStrategy().sendRedirect(request, response, redirectUri);
 
         } catch (Exception e) {
             log.error("로그인 성공 후 토큰 발급/리디렉션 중 예외 발생", e);
