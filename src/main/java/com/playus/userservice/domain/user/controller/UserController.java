a95dd129ac3e6bbcf5e541e15a6005cc6b829ec8
Minminminwoo <alsdnsms07@naver.com>
2025-05-16 09:19:40 +0900

    refactor: 중복 닉네임 검증 추가

--- src/main/java/com/playus/userservice/domain/user/controller/UserController.java
+++ src/main/java/com/playus/userservice/domain/user/controller/UserController.java
@@ -1,32 +1,32 @@
-//package com.playus.userservice.domain.user.controller;
-//
-//import jakarta.validation.Valid;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//
-//import org.springframework.web.bind.annotation.*;
-//
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import com.playus.userservice.domain.oauth.dto.CustomOAuth2User;
-//import com.playus.userservice.domain.user.dto.NicknameDto;
-//import com.playus.userservice.domain.user.service.UserService;
-//
-//@RestController
-//@RequestMapping("/user")
-//@RequiredArgsConstructor
-//public class UserController {
-//
-//    private final UserService userService;
-//
-//
-//    @PutMapping("/nickname")
-//    public ResponseEntity<NicknameDto.NicknameResponse> updateNickname(
-//            @AuthenticationPrincipal CustomOAuth2User principal,
-//            @Valid @RequestBody NicknameDto.NicknameRequest request) {
-//
-//        Long userId = Long.parseLong(principal.getName());
-//        NicknameDto.NicknameResponse resp = userService.updateNickname(userId, request);
-//        return ResponseEntity.ok(resp);
-//    }
-//
-//}
+package com.playus.userservice.domain.user.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.playus.userservice.domain.oauth.dto.CustomOAuth2User;
+import com.playus.userservice.domain.user.dto.NicknameDto;
+import com.playus.userservice.domain.user.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @PutMapping("/nickname")
+    public ResponseEntity<NicknameDto.NicknameResponse> updateNickname(
+            @AuthenticationPrincipal CustomOAuth2User principal,
+            @Valid @RequestBody NicknameDto.NicknameRequest request) {
+
+        Long userId = Long.parseLong(principal.getName());
+        NicknameDto.NicknameResponse resp = userService.updateNickname(userId, request);
+        return ResponseEntity.ok(resp);
+    }
+
+}
--- src/main/java/com/playus/userservice/domain/user/service/UserService.java
+++ src/main/java/com/playus/userservice/domain/user/service/UserService.java
@@ -17,12 +17,21 @@ public class UserService {
 
     @Transactional
     public NicknameDto.NicknameResponse updateNickname(Long userId, NicknameDto.NicknameRequest req) {
+
+        String newNickname = req.getNickname().trim();
+
+        // 사용자 조회
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResponseStatusException(
                         HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
 
-        user.updateNickname(req.getNickname());
-
+        // 닉네임 중복 확인 (db에 존재 & 본인 닉네임 아님)
+        if (userRepository.existsByNickname(newNickname)
+                && !newNickname.equals(user.getNickname())) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다.");
+        }
+        user.updateNickname(newNickname);
         return new NicknameDto.NicknameResponse(true, "닉네임이 성공적으로 변경되었습니다.");
     }
 }
