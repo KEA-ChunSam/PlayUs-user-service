a2c8179f6f32389f2ba6dceba14ceee341b77b18
Minminminwoo <alsdnsms07@naver.com>
2025-05-17 17:15:21 +0900

    refactor: serviceTest 위한 코드 추가

--- src/test/java/com/playus/userservice/IntegrationTestSupport.java
+++ src/test/java/com/playus/userservice/IntegrationTestSupport.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
