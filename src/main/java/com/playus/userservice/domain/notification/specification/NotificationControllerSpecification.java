c9872d719f909bda47f347933cbd78b34ba1ae95
Minminminwoo <alsdnsms07@naver.com>
2025-05-24 13:53:28 +0900

    refactor: 알림 컨트롤러 swagger 코드 추가

--- src/main/java/com/playus/userservice/domain/notification/specification/NotificationControllerSpecification.java
+++ src/main/java/com/playus/userservice/domain/notification/specification/NotificationControllerSpecification.java
@@ -2,6 +2,8 @@ package com.playus.userservice.domain.notification.specification;
 
 import com.playus.userservice.domain.notification.dto.response.NotificationResponse;
 import com.playus.userservice.domain.oauth.dto.CustomOAuth2User;
+import com.playus.userservice.domain.user.feign.response.CommentNotificationEvent;
+import com.playus.userservice.domain.user.feign.response.PartyNotificationEvent;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -201,4 +203,35 @@ public interface NotificationControllerSpecification {
             @ApiResponse(responseCode = "500", description = "서버 내부 오류")
     })
     ResponseEntity<Void> deleteByCommentId(Long commentId);
+
+
+    @Operation(
+            summary     = "댓글 알림 생성",
+            description = "커뮤니티 서비스에서 댓글이 작성될 때 호출되어 알림을 생성합니다.",
+            security    = @SecurityRequirement(name = "InternalServiceKey")
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "알림 생성 완료"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류",
+                    content = @Content(mediaType = APPLICATION_JSON_VALUE))
+    })
+    ResponseEntity<Void> createCommentNotification(
+            @Parameter(description = "댓글 알림 이벤트", required = true)
+            CommentNotificationEvent event
+    );
+
+    @Operation(
+            summary     = "직관팟 알림 생성",
+            description = "TWP 서비스에서 직관팟 관련 이벤트가 발생했을 때 호출되어 알림을 생성합니다.",
+            security    = @SecurityRequirement(name = "InternalServiceKey")
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "알림 생성 완료"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류",
+                    content = @Content(mediaType = APPLICATION_JSON_VALUE))
+    })
+    ResponseEntity<Void> createPartyNotification(
+            @Parameter(description = "직관팟 알림 이벤트", required = true)
+            PartyNotificationEvent event
+    );
 }
