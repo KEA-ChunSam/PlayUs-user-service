c627ecab507bb0c5929cc853c1f7bd941bb8bfcb
Minminminwoo <alsdnsms07@naver.com>
2025-05-25 13:49:05 +0900

    refactor: Notification에 있는 title, content의 최대 길이와 통일

--- src/main/java/com/playus/userservice/domain/notification/dto/request/NoticeRequest.java
+++ src/main/java/com/playus/userservice/domain/notification/dto/request/NoticeRequest.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.Size;
 
 public record NoticeRequest(
 	@NotBlank(message = "제목은 필수 입력값입니다")
-	@Size(max = 100, message = "제목은 100자 이내로 입력해주세요")
+	@Size(max = 255, message = "제목은 255자 이내로 입력해주세요")
 	String title,
 
 	@NotBlank(message = "내용은 필수 입력값입니다")
-	@Size(max = 1000, message = "내용은 1000자 이내로 입력해주세요")
+	@Size(max = 500, message = "내용은 500자 이내로 입력해주세요")
 	String content
 ) {
 }
