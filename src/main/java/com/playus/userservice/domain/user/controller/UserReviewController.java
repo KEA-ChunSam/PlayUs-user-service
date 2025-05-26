3547d5089b00f60d76dc49fcecc70f8c4037c3d0
Minminminwoo <alsdnsms07@naver.com>
2025-05-26 11:58:05 +0900

    feature: 유저 리뷰 컨트롤러 추가

---
+++ src/main/java/com/playus/userservice/domain/user/controller/UserReviewController.java
@@ -0,0 +1,32 @@
+package com.playus.userservice.domain.user.controller;
+
+import com.playus.userservice.domain.oauth.dto.CustomOAuth2User;
+import com.playus.userservice.domain.user.dto.UserReviewRequest;
+import com.playus.userservice.domain.user.dto.nickname.NicknameResponse;
+import com.playus.userservice.domain.user.service.UserReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users/reviews")
+@RequiredArgsConstructor
+public class UserReviewController {
+
+    private final UserReviewService userReviewService;
+
+    @PostMapping
+    public ResponseEntity<List<UserReviewRequest>> createUserReviews(
+            @AuthenticationPrincipal CustomOAuth2User principal,
+            @RequestBody List<UserReviewRequest> requests) {
+
+        Long userId = Long.parseLong(principal.getName());
+
+        List<UserReviewRequest> resp = userReviewService.processReviews(userId, requests);
+        return ResponseEntity.ok(resp);
+    }
+}
