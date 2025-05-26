48d532445811627b6c2bcf0aed6476a2bef2b89a
Minminminwoo <alsdnsms07@naver.com>
2025-05-26 11:27:10 +0900

    feature : 유저 태그 repository 추가

---
+++ src/main/java/com/playus/userservice/domain/user/dto/UserReviewRequest.java
@@ -0,0 +1,7 @@
+package com.playus.userservice.domain.user.dto;
+
+public record UserReviewRequest(
+        Long userId,
+        Long tagId,
+        boolean positive    // true면, +0.01 &  태그 저장, false면 –0.01 & 태그 저장 안 함
+) { }
