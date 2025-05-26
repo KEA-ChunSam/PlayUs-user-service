d89a21e0c352f3610cd995df5535a3ca30ce4253
Minminminwoo <alsdnsms07@naver.com>
2025-05-26 12:36:59 +0900

    feature: 유저 리뷰 서비스 코드 추가

---
+++ src/main/java/com/playus/userservice/domain/user/service/UserReviewService.java
@@ -0,0 +1,49 @@
+package com.playus.userservice.domain.user.service;
+
+import com.playus.userservice.domain.user.dto.UserReviewRequest;
+import com.playus.userservice.domain.user.entity.*;
+import com.playus.userservice.domain.user.repository.write.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserReviewService {
+
+    private final UserRepository userRepository;
+    private final TagRepository tagRepository;
+    private final UserTagRepository userTagRepository;
+
+    private static final float score = 0.01f;
+
+    @Transactional
+    public List<UserReviewRequest> processReviews(Long reviewerId, List<UserReviewRequest> requests) {
+
+        userRepository.findById(reviewerId)
+                .orElseThrow(() -> new IllegalArgumentException("리뷰 요청 유저가 존재하지 않습니다. id=" + reviewerId));
+
+
+        for (UserReviewRequest dto : requests) {
+            User user = userRepository.findById(dto.userId())
+                    .orElseThrow(() -> new IllegalArgumentException("리뷰 대상 유저가 존재하지 않습니다. id=" + dto.userId()));
+
+            if (dto.positive()) {
+
+                user.updateUserScore(user.getUserScore() + score);
+                Tag tag = tagRepository.findById(dto.tagId())
+                        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 태그입니다. id=" + dto.tagId()));
+                userTagRepository.save(UserTag.create(user, tag));
+
+            } else {
+                user.updateUserScore(user.getUserScore() - score);
+            }
+        }
+
+        return requests;
+
+    }
+
+}
