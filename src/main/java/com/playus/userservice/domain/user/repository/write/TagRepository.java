8eaae02106f71ed83cfb825af348b9c63b0cf39a
Minminminwoo <alsdnsms07@naver.com>
2025-05-28 15:28:48 +0900

    refactor: EOF 준수

--- src/main/java/com/playus/userservice/domain/user/repository/write/TagRepository.java
+++ src/main/java/com/playus/userservice/domain/user/repository/write/TagRepository.java
@@ -3,4 +3,6 @@ package com.playus.userservice.domain.user.repository.write;
 import com.playus.userservice.domain.user.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> { }
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
+
