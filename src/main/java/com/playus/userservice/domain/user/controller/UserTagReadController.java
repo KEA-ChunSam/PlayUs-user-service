c321a210824a1461be912df8430d4ae3d349c363
Minminminwoo <alsdnsms07@naver.com>
2025-05-29 12:01:43 +0900

    refactor: api 주소 변경

--- src/main/java/com/playus/userservice/domain/user/controller/UserTagReadController.java
+++ src/main/java/com/playus/userservice/domain/user/controller/UserTagReadController.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users/{user-id}/tags")
+@RequestMapping("/user/{user-id}/tags")
 @RequiredArgsConstructor
 public class UserTagReadController implements UserTagReadControllerSpecification  {
 
