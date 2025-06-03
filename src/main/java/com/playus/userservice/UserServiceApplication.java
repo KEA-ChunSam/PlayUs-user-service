fcd3d480962da8c0e79db5c7a23571824dc588d5
Kim-Yukyung <yukyung116@gmail.com>
2025-06-03 20:26:23 +0900

    🔥 Import 정리

--- src/main/java/com/playus/userservice/UserServiceApplication.java
+++ src/main/java/com/playus/userservice/UserServiceApplication.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SecurityScheme(
 		name      = "AccessCookie",
